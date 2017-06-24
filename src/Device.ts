821897377d7f4abd69e0f59276596ea054b6bf25
FantasticFiasco <mattias.kindborg@gmail.com>
2017-06-25 01:01:11 +0200

    wip

--- src/Device.ts
+++ src/Device.ts
@@ -2,4 +2,14 @@
  * Class describing a device on the network.
  */
 export class Device {
+    constructor(
+        /**
+         * The address.
+         */
+        readonly address: string,
+        /**
+         * The serial number.
+         */
+        readonly serialNumber: string | undefined) {
+        }
 }
