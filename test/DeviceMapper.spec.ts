1fb8e929fe6e7b00783cf5bf3cd95f78b8ad35f0
FantasticFiasco <mattias.kindborg@gmail.com>
2017-06-28 21:01:19 +0200

    wip

---
+++ test/DeviceMapper.spec.ts
@@ -0,0 +1,25 @@
+import * as bonjour from 'bonjour';
+import * as chai from 'chai';
+
+chai.should();
+
+describe('DeviceMapper', () => {
+    describe('#fromService', () => {
+
+        it('should map a service to a device');
+
+        it('should not map a service without addressses');
+
+        it('should not map a service with addressses of wrong type');
+
+        it('should not map a service with empty addressses');
+
+        it('should not map a service without address');
+
+        it('should not map a service without link local address');
+
+        it('should not map a service without MAC address');
+
+        it('should not map a service with MAC address of wrong type');
+    });
+});
