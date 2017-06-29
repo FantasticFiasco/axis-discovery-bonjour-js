e3509ea2de08677833c958a2052e935df640310e
FantasticFiasco <mattias.kindborg@gmail.com>
2017-06-29 23:20:19 +0200

    wip

--- test/AxisService.ts
+++ test/AxisService.ts
@@ -2,32 +2,42 @@ import * as bonjour from 'bonjour';
 import * as events from 'events';
 
 export class AxisService extends events.EventEmitter implements bonjour.Service {
-    constructor(addresses: string[], name: string, port: number, macAddress: string| undefined) {
+    constructor(addresses: string[], name: string, port: number, macAddress: string | undefined) {
         super();
 
         this.addresses = addresses;
         this.name = name;
         this.port = port;
-
-        if (macAddress) {
-            this.txt = {
-                macaddress: macAddress
-            };
-        } else {
-            this.txt = {}
-        }
+        this.macAddress = macAddress;
     }
+
+    private macAddress: string | undefined;
 
     addresses: string[];
     name: string;
     type: string = 'axis-video';
     subtypes: string[] = [];
-    protocol: string = 'udp';
-    host: string = '';
+    protocol: string = 'tcp';
     port: number;
-    fqdn: string = '';
-    txt: Object;
     published: boolean = true;
+
+    get fqdn(): string {
+        return `${this.name}._axis-video._tcp.local'`;
+    };
+
+    get host(): string {
+        return `axis-${(this.macAddress as string).toLowerCase()}.local`;
+    }
+
+    get txt(): Object {
+        if (this.macAddress) {
+            return {
+                macaddress: this.macAddress
+            };
+        }
+
+        return {};
+    }
 
     stop(_: () => any): void {}
     start(): void {}
