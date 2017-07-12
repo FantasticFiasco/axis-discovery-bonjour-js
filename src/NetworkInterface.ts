7da20287b7a4c6f10c391c3afd6d143e2139f99f
Mattias Kindborg @FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2017-07-12 23:21:35 +0200

    fix: multi interface support (#17)
    Replaced dependency to 'bonjour' with 'FantasticFiasco/bonjour', which is a fork with an updated dependency pointing to a new fork of 'multicast-dns', that in turn has been updated to support multiple network interfaces.
    
    Fixes #10

---
+++ src/NetworkInterface.ts
@@ -0,0 +1,20 @@
+import * as os from 'os';
+
+/**
+ * Returns all public IPv4 addresses from all network interfaces.
+ */
+export function getIPv4Addresses(): string[] {
+    const interfaces = os.networkInterfaces();
+    const addresses: string[] = [];
+
+    Object.keys(interfaces).forEach((interfaceIndex) => {
+        Object.keys(interfaces[interfaceIndex]).forEach((addressIndex) => {
+            const address = interfaces[interfaceIndex][addressIndex];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        });
+    });
+
+    return addresses;
+}
